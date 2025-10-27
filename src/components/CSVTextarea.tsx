81b4909f0a631b2fbe05ce1286364e1e6f5b6ba5
STS <serbinats@gmail.com>
2025-10-27 16:41:04 +0200

    move styles files to the styles folder

--- src/components/CSVTextarea.tsx
+++ src/components/CSVTextarea.tsx
@@ -3,7 +3,6 @@ import * as React from 'react';
 const { memo } = React;
 
 const textareaPlaceholder = `Paste CSV content here. For example:
-
 month,number
 June,10
 July,30
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,4 @@
 import { memo } from 'react';
-import './styles.css';
 
 export const Footer = memo(() => {
   return (
